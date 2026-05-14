6eb45b94f74eae680bbde88f22a6f199ca85782d
Datoix <datoix@hotmail.com>
2026-05-14 23:56:54 +0200

    feat: introduce 'state' module with 'ProjectState' class to save/load from file

---
+++ state.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+from grid import Grid
+
+class ProjectState:
+    """Obsługa stanu projektu - obsługa plików"""
+
+    def __init__(self, project_path, name, grid):
+        self.project_path = project_path
+        self.name = str(name)
+
+        self.grid = grid
+        # self.settings later
+
+    @classmethod
+    def from_file(cls, project_path):
+        """Odczyt z pliku .npz"""
+        
+        with np.load(project_path, allow_pickle=True) as bundle:
+            name = bundle["name"].item()
+            grid_data = bundle["grid"]
+
+            return cls(
+                project_path,
+                name,
+                Grid(grid_data)
+            )
+
+    def save(self):
+        """Zapis do pliku"""
+
+        path = self.project_path
+
+        if not path.endswith('.npz'):
+            path += '.npz'
+
+        np.savez(
+            path, 
+            name=self.name, 
+            grid=self.grid.data
+        )
