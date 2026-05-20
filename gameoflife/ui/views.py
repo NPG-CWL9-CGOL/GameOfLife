97d27cd2d8a6c0a696a10033c0f20dbd99beb6a5
Datoix <datoix@hotmail.com>
2026-05-20 20:10:47 +0200

    wip: modify ui system - add 'views.py' module

---
+++ gameoflife/ui/views.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass
+
+import gameoflife.config as config
+
+@dataclass
+class ViewRect:
+    x: int
+    y: int
+    width: int
+    height: int
+
+    color: tuple[int, int, int]
+    border_color: tuple[int, int, int]
+    border_radius: int
+
+
+@dataclass
+class ViewText:
+    text: set
+    x: int
+    y: int
+    
+    color: tuple[int, int, int] = config.TEXT_COLOR
+
+
+@dataclass
+class ViewBackgroundColor:
+    color: tuple[int, int, int]
