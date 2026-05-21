99c7b9dce44b826308c78b61851d8d7235490d6b
Datoix <datoix@hotmail.com>
2026-05-21 21:13:29 +0200

    chore: remove redundant imports from 'main.py'

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import pygame
-import random
-from gameoflife import config
 from gameoflife.app import App
-from gameoflife.grid import GridData, GridGeometry
-from gameoflife.math_utils import point_in_rect
-from gameoflife.settings import AppSettings
 
 def main():
     app = App.create()
